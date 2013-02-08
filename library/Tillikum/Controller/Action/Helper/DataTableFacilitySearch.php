fcab958d40de53dfd57e154124fdbc14115d8be0
Gordon Stratton <gordon.stratton@gmail.com>
2013-02-08 14:22:32 -0800

    Add capability for facility search, and remove some replaced cruft

---
+++ library/Tillikum/Controller/Action/Helper/DataTableFacilitySearch.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * The Tillikum Project (http://tillikum.org/)
+ *
+ * @link       http://tillikum.org/websvn/
+ * @copyright  Copyright 2009-2012 Oregon State University (http://oregonstate.edu/)
+ * @license    http://www.gnu.org/licenses/gpl-2.0-standalone.html GPLv2
+ */
+
+namespace Tillikum\Controller\Action\Helper;
+
+use DateTime;
+use Zend_Controller_Action_Helper_Abstract as AbstractHelper;
+
+class DataTableFacilitySearch extends AbstractHelper
+{
+    public function dataTableFacilitySearch(array $data)
+    {
+        $ac = $this->_actionController;
+
+        $actions = array(
+            'view' => $ac->getAcl()->isAllowed('_user', 'facility', 'read')
+        );
+
+        $rows = array();
+        foreach ($data as $datum) {
+            $config = $datum[0];
+
+            $row = array(
+                'actions' => $actions,
+                'availableSpace' => $datum['availableSpace'],
+                'bookingCount' => $datum['bookingCount'],
+                'capacity' => $config->capacity,
+                'end' => $config->end ?: new DateTime('2099-01-01'),
+                'facility_group' => $config->facility->facility_group->getConfigOnDate($config->start)->name,
+                'facility' => $config->name,
+                'gender' => $config->gender,
+                'id' => $config->id,
+                'start' => $config->start ?: new DateTime('1900-01-01'),
+                'suite' => $config->suite,
+                'note' => $config->note,
+                'tags' => $config->tags,
+                'view_uri' => $ac->getHelper('Url')->direct(
+                    'view',
+                    'facility',
+                    'facility',
+                    array(
+                        'id' => $config->facility->id,
+                    )
+                ),
+            );
+
+            $rows[] = $row;
+        }
+
+        return $rows;
+    }
+
+    public function direct(array $data)
+    {
+        return $this->dataTableFacilitySearch($data);
+    }
+}
---
+++ library/Tillikum/Form/Facility/Search.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ * The Tillikum Project (http://tillikum.org/)
+ *
+ * @link       http://tillikum.org/websvn/
+ * @copyright  Copyright 2009-2012 Oregon State University (http://oregonstate.edu/)
+ * @license    http://www.gnu.org/licenses/gpl-2.0-standalone.html GPLv2
+ */
+
+namespace Tillikum\Form\Facility;
+
+use DateTime;
+use Doctrine\ORM\EntityManager;
+
+class Search extends \Tillikum_Form
+{
+    protected $entityManager;
+
+    public function init()
+    {
+        parent::init();
+
+        $date = new \Tillikum_Form_Element_Date(
+            'date',
+            array(
+                'label' => 'Facility configuration date',
+                'required' => true,
+            )
+        );
+
+        $facilityGroupIds = new \Zend_Form_Element_Multiselect(
+            'facilitygroup_ids',
+            array(
+                'label' => 'Facility group',
+                'multiOptions' => array(),
+            )
+        );
+
+        $gender = new \Zend_Form_Element_Text(
+            'gender',
+            array(
+                'label' => 'Gender',
+            )
+        );
+
+        $capacity = new \Tillikum_Form_Element_Number(
+            'capacity',
+            array(
+                'label' => 'Capacity',
+                'validators' => array(
+                    new \Zend_Validate_GreaterThan(0),
+                ),
+            )
+        );
+
+        $availableSpace = new \Tillikum_Form_Element_Number(
+            'available_space',
+            array(
+                'label' => 'Spaces available (at minimum)',
+                'validators' => array(
+                    new \Zend_Validate_GreaterThan(0),
+                ),
+            )
+        );
+
+        $tags = new \Zend_Form_Element_Multiselect(
+            'tags',
+            array(
+                'label' => 'Tags',
+                'multiOptions' => array(),
+            )
+        );
+
+        $this->addElements(
+            array(
+                $date,
+                $facilityGroupIds,
+                $gender,
+                $capacity,
+                $availableSpace,
+                $tags,
+                $this->createSubmitElement(array('label' => 'Search')),
+            )
+        );
+    }
+
+    public function setEntityManager(EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+
+        $rows = $this->entityManager->createQuery(
+            '
+            SELECT fg.id, fgc.name
+            FROM Tillikum\Entity\FacilityGroup\FacilityGroup fg
+            JOIN fg.configs fgc
+            WHERE :now BETWEEN fgc.start AND fgc.end
+            ORDER BY fgc.name
+            '
+        )
+            ->setParameter('now', new DateTime())
+            ->getResult();
+
+        $multiOptions = array();
+        foreach ($rows as $row) {
+            $multiOptions[$row['id']] = $row['name'];
+        }
+
+        $this->facilitygroup_ids->setMultiOptions($multiOptions);
+        $this->facilitygroup_ids->setAttrib('size', min(10, count($multiOptions)));
+
+        $rows = $this->entityManager->createQuery(
+            '
+            SELECT t.id, t.name
+            FROM Tillikum\Entity\Facility\Config\Tag t
+            WHERE t.is_active = :isActive
+            ORDER BY t.name
+            '
+        )
+            ->setParameter('isActive', true)
+            ->getResult();
+
+        $multiOptions = array();
+        foreach ($rows as $row) {
+            $multiOptions[$row['id']] = $row['name'];
+        }
+
+        $this->tags->setMultiOptions($multiOptions);
+        $this->tags->setAttrib('size', min(10, count($multiOptions)));
+    }
+}
